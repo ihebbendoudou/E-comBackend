d599ab6db34271a9eceea332e76526eb9280c786
Bouzezi <bouzeziahmed5@gmail.com>
2023-05-07 12:52:20 +0100

    resolve conflict

--- src/main/java/com/project/IshopPfe/ProductApplication.java
+++ src/main/java/com/project/IshopPfe/ProductApplication.java
@@ -5,12 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan
+@ComponentScan("com.project.IshopPfe")
 @SpringBootApplication
 @EnableJpaRepositories
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
     }
-
 }
