bf6507c18c0c65cd6213e2ca59d6e275c1279ef9
Bouzezi <bouzeziahmed5@gmail.com>
2023-05-14 16:25:11 +0100

    add cross origin

--- src/main/java/com/project/IshopPfe/Controller/SteLivraisonController.java
+++ src/main/java/com/project/IshopPfe/Controller/SteLivraisonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "api/stelivraison")
 public class SteLivraisonController {
